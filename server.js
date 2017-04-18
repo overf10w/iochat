3219496094bf1dce16dc0c3b83b9d401d7797c0e
pwnzr <pvvner@yandex.ru>
2017-04-19 01:08:48 +0300

    Keep track of connected and disconnected 'users'

--- server.js
+++ server.js
@@ -19,6 +19,9 @@ io.sockets.on('connection', function (socket) {
     console.log('Connected: %s sockets connected', connections.length);
 
     // Disconnect
-    connections.splice(connections.indexOf(socket), 1);
-    console.log('Disconnected: %s sockets connected', connections.length);
+    socket.on('disconnect', function (data) {
+        connections.splice(connections.indexOf(socket), 1);
+        console.log('Disconnected: %s sockets connected', connections.length);
+    });
+
 });
